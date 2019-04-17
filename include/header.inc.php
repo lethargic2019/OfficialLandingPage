352a57f3800612d89481f2752dad8ce02c9cdd0b
lethargic2019 <49717322+lethargic2019@users.noreply.github.com>
2019-04-17 18:10:41 +0530

    Add files via upload

---
+++ include/header.inc.php
@@ -0,0 +1,48 @@
+<div class="uk-navbar-container" uk-navbar>
+  <div class="uk-container uk-container-center">
+    <div class="uk-navbar-left">
+      <ul class="uk-navbar-nav">
+          <li class="uk-active"><a href="#">Home</a></li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Board of directors</a></li>
+          <li>
+              <a href="#">Loans</a>
+              <div class="uk-navbar-dropdown">
+                  <ul class="uk-nav uk-navbar-dropdown-nav">
+                      <li class="uk-active(*)"><a href="#">Loan Rules</a></li>
+                      <li><a href="#">How to Apply</a></li>
+                      <li><a href="#">Emergency Loans</a></li>
+                  </ul>
+              </div>
+          </li>
+          <li>
+              <a href="#">Deposit</a>
+              <div class="uk-navbar-dropdown">
+                  <ul class="uk-nav uk-navbar-dropdown-nav">
+                      <li class="uk-active(*)"><a href="#">Monthly Income Scheme</a></li>
+                      <li><a href="#">Recurring Deposit Scheme</a></li>
+                      <li><a href="#">Simple Fix Deposit</a></li>
+                  </ul>
+              </div>
+          </li>
+          <li><a href="#">Financial Status</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#" class="uk-clearfix uk-float-right">Member login</a></li>
+      </ul>
+
+    </div>
+  </div>
+  <div class="uk-navbar-flip uk-hidden-small">
+      <ul class="uk-navbar-nav">
+          <li><a href=""><span uk-icon="info"></span></a></li>
+      </ul>
+  </div>
+  <nav class="uk-navbar uk-navbar-container uk-margin uk-visible-small">
+    <div class="uk-navbar-left">
+      <a class="uk-navbar-toggle uk-visible-small" uk-navbar-toggle-icon href="#offcanvas-nav" data-uk-offcanvas></a>
+    </div>
+  </nav>
+  <div id="offcanvas-nav" class="uk-offcanvas">
+      <div class="uk-offcanvas-bar">...</div>
+  </div>
+</div>
